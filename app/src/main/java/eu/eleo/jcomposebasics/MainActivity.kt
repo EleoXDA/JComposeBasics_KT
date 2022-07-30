c3226d99aca5d91e3af5bf0db3e537346be8d3a7
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-07-30 23:36:34 +0200

    Create two rows and adjust the text position

--- app/src/main/java/eu/eleo/jcomposebasics/MainActivity.kt
+++ app/src/main/java/eu/eleo/jcomposebasics/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -32,20 +33,28 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@Composable
+private fun JCB(names: List<String> = listOf("World", "Compose")){
+    Surface(color = MaterialTheme.colors.background) {
+        Column(modifier = Modifier.padding(vertical = 4.dp),
+            ) {
+            for (name in names) {
+                Greeting(name = name)
+            }
+        }
+    }
+}
 
 @Composable
 private fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-    Column {
-        Text(text = "Hello,", modifier = Modifier.padding(24.dp))
-        Text(text = "$name!", modifier = Modifier.padding(24.dp))
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Column (modifier = Modifier.fillMaxWidth().padding(24.dp)) {
+            Text(text = "Hello,")
+            Text(text = "$name!")
     }
-    }
-}
-@Composable
-private fun JCB() {
-    Surface(color = MaterialTheme.colors.background) {
-        Greeting("Android")
     }
 }
 
