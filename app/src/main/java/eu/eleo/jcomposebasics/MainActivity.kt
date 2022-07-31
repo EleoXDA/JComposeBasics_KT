f7383dfb8452ebe7089c9c77607b0d5d5063fcb9
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-07-31 12:46:33 +0200

    Add an onboarding screen to the app

--- app/src/main/java/eu/eleo/jcomposebasics/MainActivity.kt
+++ app/src/main/java/eu/eleo/jcomposebasics/MainActivity.kt
@@ -3,14 +3,8 @@ package eu.eleo.jcomposebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -18,6 +12,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import eu.eleo.jcomposebasics.ui.theme.JComposeBasicsTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 
 
 class MainActivity : ComponentActivity() {
@@ -37,13 +34,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 @Composable
-private fun JCB(names: List<String> = listOf("World", "Compose")){
-    Surface(color = MaterialTheme.colors.background) {
-        Column(modifier = Modifier.padding(vertical = 4.dp),
-            ) {
-            for (name in names) {
-                Greeting(name = name)
-            }
+fun JCB() {
+    Greetings()
+}
+
+@Composable
+private fun Greetings(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
         }
     }
 }
@@ -69,6 +68,33 @@ private fun Greeting(name: String) {
             }
 
     }
+    }
+}
+@Composable
+fun OnboardingScreen() {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = { shouldShowOnboarding = false }
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    JComposeBasicsTheme {
+        OnboardingScreen()
     }
 }
 
