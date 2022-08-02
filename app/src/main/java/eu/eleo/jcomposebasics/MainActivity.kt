662146b84eb54d0743e6f40ff6ca25c7ee712312
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-08-02 21:29:51 +0200

    Change color styling in light/night modes

--- app/src/main/java/eu/eleo/jcomposebasics/MainActivity.kt
+++ app/src/main/java/eu/eleo/jcomposebasics/MainActivity.kt
@@ -1,5 +1,6 @@
 package eu.eleo.jcomposebasics
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -112,7 +113,12 @@ private fun Greeting(name: String) {
     }
 }
 
-
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 
 @Preview(showBackground = true)
 @Composable
