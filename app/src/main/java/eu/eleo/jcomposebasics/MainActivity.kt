b1727a40a90aae19213fdd715151a0dfb34e8b3c
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-08-02 20:29:54 +0200

    Add animation for the columns

--- app/src/main/java/eu/eleo/jcomposebasics/MainActivity.kt
+++ app/src/main/java/eu/eleo/jcomposebasics/MainActivity.kt
@@ -3,6 +3,7 @@ package eu.eleo.jcomposebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -76,8 +77,12 @@ private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
 
 @Composable
 private fun Greeting(name: String) {
+
     val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    val extraPadding by animateDpAsState(
+        if (expanded.value) 48.dp else 0.dp
+    )
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
