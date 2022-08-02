cfdd2dc261d990b1df8cc49444d132a107318648
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-08-02 20:38:28 +0200

    Change styling of fonts

--- app/src/main/java/eu/eleo/jcomposebasics/MainActivity.kt
+++ app/src/main/java/eu/eleo/jcomposebasics/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import eu.eleo.jcomposebasics.ui.theme.JComposeBasicsTheme
@@ -97,7 +98,9 @@ private fun Greeting(name: String) {
             Column (modifier = Modifier.weight(1f).padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold)
+                )
             }
             OutlinedButton(
                 onClick = { expanded.value = !expanded.value },
